7153112a838711510166b5c5b7cd151f3de3b5ea
mge19 <mge42455@gmail.com>
2021-11-21 20:45:04 +0300

    Ödev 1

---
+++ src/Odev1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Odev1 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        double boy=input.nextFloat();
+        System.out.println("Lütfen kilonuzu giriniz :");
+        int kilo=input.nextInt();
+        double bmi=kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+bmi);
+    }
+}
